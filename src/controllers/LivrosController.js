6843f474bdb35372aeb38b8279de855ccb66bb90
Artur Moreira <arturacm@gmail.com>
2021-07-24 10:23:49 -0300

    implementação do POO no router da LivrosController

--- src/controllers/LivrosController.js
+++ src/controllers/LivrosController.js
@@ -5,33 +5,42 @@ const router = express.Router();
 ///importando o Livro
 const livro = require("../models/LivrosModel")
 
-//todos os livros
-router.get ("/livros", (req, res) => {
-    livro.findAll()//metodo do sequelize para retornar tudo
-    .then((dados)=>{
-        res.json(dados)
-    })
-    .catch(e=>{
-        res.sendStatus(e)
-    })
-});
+//
+class LivrosController{
+    constructor(router){
+        //todos os livros
+        router.get ("/livros", this.getAllLivros);
 
-// inserir livros
-router.post ("/livros/", (req, res) => {
-    let {nome,preco,autor,genero} = req.body;
-    //metodo para criar/post
-    livro.create({
-        nome: nome,
-        preco:preco,
-        autor:autor,
-        genero:genero
-    }).then((dados)=>{
-        res.send(dados)
-    }).catch(e=>{
-        res.sendStatus(e)
-    })
-});
+        // inserir livros
+        router.post ("/livros/", this.insertOneLivro);
 
+    }
+    getAllLivros = (req, res) => {
+        livro.findAll()//metodo do sequelize para retornar tudo
+        .then((dados)=>{
+            res.json(dados)
+        })
+        .catch(e=>{
+            res.sendStatus(e)
+        })
+    }
+    insertOneLivro = (req, res) => {
+        let {nome,preco,autor,genero} = req.body;
+        //metodo para criar/post
+        livro.create({
+            nome: nome,
+            preco:preco,
+            autor:autor,
+            genero:genero
+        }).then((dados)=>{
+            res.send(dados)
+        }).catch(e=>{
+            res.sendStatus(e)
+        })
+    }
+
+}
+const livroController = new LivrosController(router)
 
 /////exportando as rotas do livro
 module.exports = router;

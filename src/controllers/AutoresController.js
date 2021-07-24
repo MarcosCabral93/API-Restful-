1f0f027b6f1dfda1a9dc386f85dcaa194113f541
Natalia Galvao <nreginagal@gmail.com>
2021-07-24 17:50:44 -0300

    criacao do controller de autores ainda vazio

---
+++ src/controllers/AutoresController.js
@@ -0,0 +1,17 @@
+class AutoresController {
+    construtor(router) {
+        //todos os autores
+    }
+    createAutor() {
+
+    }
+    getAllAutores() {
+
+    }
+    patchAutor(autor_id) {
+
+    }
+    deleteAutor(autor_id) {
+
+    }
+}

08084b7ddd41c5e17896464ac586b0b2e61c9ee6
Natalia Galvao <nreginagal@gmail.com>
2021-07-27 01:45:54 -0300

    WIP fazer funcionar as notas abstraidas

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const express = require("express")//importando e Express
+const express = require("express") //importando e Express
 const app = express() //instanciando o express
+    //
+const routes = require("./src/routes")
 
-app.use(express.json())// usando o middleware json
-const {APP_PORT,APP_NAME}=require('./src/config/config')//importando o nome da rota e a porta
+app.use(express.json()) // usando o middleware json
+const { APP_PORT, APP_NAME } = require('./src/config/config') //importando o nome da rota e a porta
 
-const Autor=require('./src/models/AutorModel')
-const Livro=require('./src/models/LivrosModel')
+routes(app)
+const Autor = require('./src/models/AutorModel')
+const Livro = require('./src/models/LivrosModel')
 
-const livroController=require('./src/controllers/LivrosController')
+const livroController = require('./src/controllers/LivrosController')
 
 //toda vez que inicia a aplicação, é verificado o bd
 const db = require("./src/infra/db")
 db.sync()
-.then(()=>console.log("banco conectado e sincronizado e pronto para uso"))
-.catch((e)=>console.log(e))
+    .then(() => console.log("banco conectado e sincronizado e pronto para uso"))
+    .catch((e) => console.log(e))
 
-app.use("/",livroController);
+app.use("/", livroController);
 
-app.listen(APP_PORT,()=>{
+app.listen(APP_PORT, () => {
     console.log(`O ${APP_NAME} monstrao ta rodando na porta  ${APP_PORT} bixo`)
 })

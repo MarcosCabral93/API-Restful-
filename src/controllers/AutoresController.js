8dcdb45c292900579bd32c9f88f36e768bd37585
Natalia Galvao <nreginagal@gmail.com>
2021-07-28 17:04:44 -0300

    construcao do campo update e delete

--- src/controllers/AutoresController.js
+++ src/controllers/AutoresController.js
@@ -23,11 +23,41 @@ class AutoresController {
                 res.sendStatus(e)
             })
     }
-    updateAutor(autor_id) {
-        //atualiza todos os autores
+    updateAutor(req, res) {
+        try {
+            autor.findByPk(autor_id)
+            const id = req.params.id
+            let { nome, nacionalidade } = req.bod;
+            const dados = await autor.update({
+                nome: nome,
+                nacionalidade: nacionalidade
+            }, {
+                where: {
+                    "id": id
+                }
+            })
+            if (dados[0]) res.send(`Autor de id= ${id} atualizado com sucesso`)
+            else res.send(`Nenhum autor de id=${id} foi atualizado`)
+        } catch (err) {
+            console.log("Algo deu errado aqui" + err)
+            res.send("Por favor, insira os dados do autor desejado novamente")
+        }
     }
-    deleteAutor(autor_id) {
-        //deleta um autor especifico
+    deleteAutor(req, res) {
+        try {
+            const dados = await autor.destroy({
+                    where: {
+                        id: id
+                    }
+                }) //metodo sequelize para deletar
+
+            if (dados) res.send(`Autor de id=${autor_id} removido com sucesso`)
+            else res.send(`Nenhum livro de id=${autor_id} foi encontrado`)
+
+        } catch (err) {
+            console.log(`Algo deu errado no erro ${autor_id}`)
+            res.send("Falha na deleção do autor")
+        }
     }
 }
 const autoresController = new AutoresController
